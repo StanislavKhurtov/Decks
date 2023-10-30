b30ffbb6c62af7f3b77b9c7d7974f5f19709d9bb
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-10-30 22:56:35 +0300

    feat:change structure

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,18 +1,9 @@
 import s from './DecksList.module.css'
-import { useEffect } from 'react'
-import { Deck } from '../decks-api.ts'
-import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
-import { fetchDecksTC } from '../../decks/decks-thunks.ts'
 import { DeckItem } from '../DecksList/DeckItem/DeckItem.tsx'
+import { useFetchDecks } from '../DecksList/useRetchDecks.ts'
 
 export const DecksList = () => {
-  const decks = useAppSelector<Deck[]>(state => state.decks.decks)
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchDecksTC())
-  }, [])
-
+  const { decks } = useFetchDecks()
   return <ul className={s.list}>
     {decks.map((deck) => (
       <DeckItem key={deck.id} deck={deck} />
