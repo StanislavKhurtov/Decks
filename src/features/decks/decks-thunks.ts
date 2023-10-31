8d80e1d5b52d16debd5f7480ff2b30b40413e7dd
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-10-31 17:37:30 +0300

    feat:change structure

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -3,18 +3,24 @@ import { decksAPI } from '../decks/decks-api.ts'
 import { fetchDecksAC } from '../decks/decks-reducer.ts'
 import { AppDispatch } from '../../app/store.ts'
 
-
-
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
   const res = await decksAPI.fetchDecks()
   dispatch(fetchDecksAC(res.data.items))
 }
-
 export const addDeckTC = (name: string) => async (dispatch: AppDispatch) => {
-  await decksAPI.addDeck(name);
-  dispatch(fetchDecksTC());
-};
+  await decksAPI.addDeck(name)
+  dispatch(fetchDecksTC())
+}
 
+export const updateDeckTC = (id: string, name: string) => async (dispatch: AppDispatch) => {
+  await decksAPI.updateDeck(id,name)
+  dispatch(fetchDecksTC())
+}
+
+export const removeDeckTC = (id: string) => async (dispatch: AppDispatch) => {
+  await decksAPI.removeDeck(id)
+  dispatch(fetchDecksTC())
+}
 
 /*export const addDeckTC = (name:string) => async (dispatch: Dispatch) => {
   const res = await decksAPI.addDeck(name)
